1635d356e259e27347bc5b189d58a896f0c0d175
Daniel Poulson <danielpoulson@Daniels-iMac.local>
2016-09-11 19:50:46 +1000

    First Commmit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+import Footer from './Footer';
+import AddTodo from './AddTodo';
+import VisibleTodoList from './VisibleTodoList';
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
+
+App.propTypes = {
+  params: PropTypes.object,
+};
+export default App;
